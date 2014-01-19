489a95413a41737c1716803c1d70ff6306d8ae48
Nathan Schleh <starius65@gmail.com>
2014-01-18 19:26:15 -0500

    Updated "Foe"

--- GlowRPGEngine/src/com/luminarii/glowrpgengine/battle/Battle.java
+++ GlowRPGEngine/src/com/luminarii/glowrpgengine/battle/Battle.java
@@ -2,7 +2,9 @@ package com.luminarii.glowrpgengine.battle;
 
 import java.util.Arrays;
 import java.util.Scanner;
+
 import com.luminarii.glowrpgengine.battle.characters.*;
+import com.luminarii.glowrpgengine.battle.foes.Foe;
 
 public class Battle {
 
@@ -10,5 +12,7 @@ public class Battle {
 		Hero.params[0]();
 		System.out.println(Arrays.toString(params));
 		return false;
+		Foe.Foe(3, 3, 3, 3, 1, 0, "Goblin");
 	}
+	
 }
--- GlowRPGEngine/src/com/luminarii/glowrpgengine/battle/foes/Foe.java
+++ GlowRPGEngine/src/com/luminarii/glowrpgengine/battle/foes/Foe.java
@@ -1,5 +1,12 @@
 package com.luminarii.glowrpgengine.battle.foes;
 
 public class Foe {
+	
+	public static void Foe(int foehp, int foesp, int foemaxhp, int foemaxsp, int foeattack, int foedefense, String foename) {
+		
+		
+		
+	}
+
 
 }
