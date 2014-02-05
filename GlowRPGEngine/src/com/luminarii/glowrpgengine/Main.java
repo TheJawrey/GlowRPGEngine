b0c8d76f11502bece25c5a0b453a414a1bcd12ca
jack.awrey <jack.awrey@CSA-3TMDF949142.iTeach>
2014-02-05 14:01:14 -0500

    Added JFrame.

--- GlowRPGEngine/src/com/luminarii/glowrpgengine/Main.java
+++ GlowRPGEngine/src/com/luminarii/glowrpgengine/Main.java
@@ -1,11 +1,19 @@
 package com.luminarii.glowrpgengine;
 
+import java.awt.BorderLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 import com.luminarii.glowrpgengine.battle.Battle;
 import com.luminarii.glowrpgengine.battle.foes.*;
 import com.luminarii.glowrpgengine.battle.heros.*;
 
-public class Main {
+public class Main{
 
 	public static void main(String args[]){
 		
@@ -14,22 +22,20 @@ public class Main {
 		party.addHero(new HeroKnight());
 		party.addHero(new HeroScout());
 		
-		Scanner scan = new Scanner(System.in);
-		boolean gameOver = false;
-		while(gameOver==false){
-			
-			System.out.println("Choose an option:");
-			System.out.println("1. Start battle with slimes");
-			
-			switch(scan.nextInt()){
-			case 1:
-				Battle.battle(null, party, new GroupSlimes());
-				break;
-			}
-		}
-		System.out.println("Game Over");
-		scan.close();
-		System.exit(0);
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("GlowRPGEngine");
+//frame events
+		Label label = new Label("Select Enemy Group:");
+		JButton slimes = new JButton("Slimes x4");
+		frame.add(label);
+		frame.add(slimes);
+//frame events		
+		frame.pack();
+		frame.setVisible(true);
+		
+
+		Battle.battle(null, party, new GroupSlimes());
 	}
 }
 	
